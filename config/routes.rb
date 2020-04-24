0a3977bf72dac521dbb3f0497aa2c6711375a189
Håkon Grov <Hakongrov@gmail.com>
2020-04-25 01:17:45 +0200

    A lot of old updates. Don't remember what they do

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get '/about', to: 'static_pages#about'
+  get '/home', to: 'static_pages#home'
   get '/login',  to: 'sessions#new'
   get '/register', to: 'users#new'
   post '/login', to: 'sessions#create'
